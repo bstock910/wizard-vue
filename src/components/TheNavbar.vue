aff63316570ba167f7c3ca3df1dd0bb4c1bf12bb
Brian Stock <brian.r.stock@gmail.com>
2019-01-10 19:25:38 -0500

    install

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar is-light">
+      <div class="container">
+        <div class="navbar-brand">
+          <a href="#"
+             class="navbar-item">
+            <img src="../assets/img/logo.png"
+                 alt="AppointMaster Logo">
+          </a>
+        </div>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar'
+}
+</script>
+
+<style scoped>
+</style>
